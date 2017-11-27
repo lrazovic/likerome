b0a16afcd0752b2265d8d37ba68f4d7c4f9122f5
Leonardo Razovic <leorazzo@gmail.com>
2017-11-27 22:30:44 +0100

    Updated Logo

--- LikeApp/ViewController.swift
+++ LikeApp/ViewController.swift
@@ -6,91 +6,89 @@
 //  Copyright © 2017 Leonardo Razovic. All rights reserved.
 //
 
-import UIKit
 import Alamofire
-import SwiftyJSON
 import AVFoundation
-
+import SwiftyJSON
+import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
-    
+
     // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
-        self.downloadedPhoto.addGestureRecognizer(longPressRecognizer)
-        self.setUI()
+        downloadedPhoto.addGestureRecognizer(longPressRecognizer)
+        setUI()
         cityPicker.dataSource = self
         cityPicker.delegate = self
         urlField.delegate = self
-        if((pasteboard.string?.range(of:"instagram")) != nil) {
+        if (pasteboard.string?.range(of: "instagram")) != nil {
             urlString = pasteboard.string!
             urlField.insertText(urlString)
         }
     }
-    
+
     // MARK: Variables
-    let pickerData = ["Torino","Roma","Milano","Bologna","Napoli"]
+    let pickerData = ["Torino", "Roma", "Milano", "Bologna", "Napoli"]
     let pasteboard = UIPasteboard.general
     var urlString: String = ""
     var pickedCity: String = ""
     var player: AVAudioPlayer?
-    
+
     // MARK: Outlets
-    @IBOutlet weak var buttonUi: UIButton!
-    @IBOutlet weak var urlField: UITextField!
-    @IBOutlet weak var cityPicker: UIPickerView!
-    @IBOutlet weak var downloadedPhoto: UIImageView!
-    @IBOutlet weak var locationText: UILabel!
-    
+    @IBOutlet var buttonUi: UIButton!
+    @IBOutlet var urlField: UITextField!
+    @IBOutlet var cityPicker: UIPickerView!
+    @IBOutlet var downloadedPhoto: UIImageView!
+    @IBOutlet var locationText: UILabel!
+
     // MARK: Functions
-    @IBAction func generateButton(_ sender: UIButton) {
-        self.buttonPressed()
+    @IBAction func generateButton(_: UIButton) {
+        buttonPressed()
     }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+
+    func numberOfComponents(in _: UIPickerView) -> Int {
         return 1
     }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+
+    func pickerView(_: UIPickerView, numberOfRowsInComponent _: Int) -> Int {
         return pickerData.count
     }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+    func pickerView(_: UIPickerView, titleForRow row: Int, forComponent _: Int) -> String? {
         pickedCity = pickerData[row]
         return pickerData[row] as String
     }
-    
-    
-    func textFieldShouldReturn(_ scoreText: UITextField) -> Bool {
-        self.view.endEditing(true)
-        self.buttonPressed()
+
+    func textFieldShouldReturn(_: UITextField) -> Bool {
+        view.endEditing(true)
+        buttonPressed()
         return true
     }
-    
-    func getPhoto(swiftyJsonVar:JSON) -> String {
+
+    func getPhoto(swiftyJsonVar: JSON) -> String {
         return swiftyJsonVar["graphql"]["shortcode_media"]["display_url"].stringValue
     }
-    
+
     func getUrl() -> String {
         return urlField.text! + "?__a=1"
     }
-    
-    func getUsername(swiftyJsonVar:JSON) -> String {
+
+    func getUsername(swiftyJsonVar: JSON) -> String {
         return swiftyJsonVar["graphql"]["shortcode_media"]["owner"]["username"].stringValue
     }
-    
-    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+
+    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
         URLSession.shared.dataTask(with: url) { data, response, error in
             completion(data, response, error)
-            }.resume()
+        }.resume()
     }
-    
+
     func downloadImage(url: URL) {
         getDataFromUrl(url: url) { data, response, error in
             guard let data = data, error == nil else { return }
             print(response?.suggestedFilename ?? url.lastPathComponent)
-            DispatchQueue.main.async() {
+            DispatchQueue.main.async {
                 self.downloadedPhoto.contentMode = .scaleAspectFill
                 self.downloadedPhoto.frame = CGRect(x: 0, y: 0, width: 320, height: 320)
                 self.downloadedPhoto.layer.cornerRadius = 24
@@ -100,21 +98,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 self.downloadedPhoto.image = UIImage(data: data)
             }
         }
-        self.playSound(name: "complete")
+        playSound(name: "complete")
     }
-    
-    @objc func longPressed(sender: UILongPressGestureRecognizer) {
-        if (self.downloadedPhoto.image != nil) {
+
+    @objc func longPressed(sender _: UILongPressGestureRecognizer) {
+        if downloadedPhoto.image != nil {
             let shareText = ""
             let vc = UIActivityViewController(activityItems: [shareText, self.downloadedPhoto.image!], applicationActivities: [])
-            self.present(vc, animated: true, completion: nil) } else { return }
+            present(vc, animated: true, completion: nil) } else { return }
     }
-    
-    func generateDescription(swiftyJsonVar:JSON)  {
+
+    func generateDescription(swiftyJsonVar: JSON) {
         let ret: String = getRightDesc(pickedCity: pickedCity, username: getUsername(swiftyJsonVar: swiftyJsonVar))
-        UIPasteboard.general.string = ret;
+        UIPasteboard.general.string = ret
     }
-    
+
     func getRightDesc(pickedCity: String, username: String) -> String {
         switch pickedCity {
         case "Roma":
@@ -127,34 +125,34 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             return "Altra Citta"
         }
     }
-    
-    func buttonPressed(){
-        //Levo la tastiera
-        self.hideKeyboardWhenTappedAround()
-        
+
+    func buttonPressed() {
+        // Levo la tastiera
+        hideKeyboardWhenTappedAround()
+
         if urlField.text!.contains("instagram") {
             let url = getUrl()
             Alamofire.request(url).validate().responseJSON { response in
                 switch response.result {
                 case .success:
-                    //Scarico JSON
+                    // Scarico JSON
                     let swiftyJsonVar = JSON(response.result.value!)
-                    
-                    //Genero la Descrcizione
+
+                    // Genero la Descrcizione
                     self.generateDescription(swiftyJsonVar: swiftyJsonVar)
-                    
-                    //Scarico la Foto e la inserisco nella UI
+
+                    // Scarico la Foto e la inserisco nella UI
                     if let url = URL(string: self.getPhoto(swiftyJsonVar: swiftyJsonVar)) {
                         self.downloadImage(url: url)
                     }
-                    
-                    //Scrivo il Geotag
+
+                    // Scrivo il Geotag
                     self.locationText.text = self.getLocation(swiftyJsonVar: swiftyJsonVar)
                     self.playSound(name: "done")
-                case .failure(_):
+                case .failure:
                     self.playSound(name: "error")
                     let alert = UIAlertController(title: "URL Instagram non valido!", message: "Inserisci un URL valido", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Riprova"), style: .`default`, handler: { _ in
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Riprova"), style: .default, handler: { _ in
                         NSLog("The \"OK\" alert occured.")
                     }))
                     self.present(alert, animated: true, completion: nil)
@@ -163,19 +161,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         } else {
             self.playSound(name: "error")
             let alert = UIAlertController(title: "URL non valido!", message: "Inserisci un URL valido", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Riprova"), style: .`default`, handler: { _ in
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Riprova"), style: .default, handler: { _ in
                 NSLog("The \"OK\" alert occured.")
             }))
-            self.present(alert, animated: true, completion: nil)
+            present(alert, animated: true, completion: nil)
         }
     }
-    
-    func getLocation(swiftyJsonVar:JSON) -> String {
+
+    func getLocation(swiftyJsonVar: JSON) -> String {
         let locationString = swiftyJsonVar["graphql"]["shortcode_media"]["location"]["name"].stringValue
-        if (locationString == "") { return "📍" + "Non Impostata" }
+        if locationString == "" { return "📍" + "Non Impostata" }
         else { return "📍" + locationString }
     }
-    
+
     func playSound(name: String) {
         guard let url = Bundle.main.url(forResource: name, withExtension: "m4a") else { return }
         do {
@@ -188,24 +186,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             print(error.localizedDescription)
         }
     }
-    
-    func setUI(){
+
+    func setUI() {
         // Button
-        self.buttonUi.layer.cornerRadius = 10
-        self.buttonUi.center = view.center
-        
+        buttonUi.layer.cornerRadius = 10
+        buttonUi.center = view.center
+
         // Picker
-        self.cityPicker.center = view.center
-        
+        cityPicker.center = view.center
+
         // Image
-        
+
         // Keyboard
-        self.hideKeyboardWhenTappedAround()
+        hideKeyboardWhenTappedAround()
     }
-    
+
     func setShadow() {
-        self.downloadedPhoto.layer.shadowOpacity = 0.45
-        self.downloadedPhoto.layer.shadowRadius = 8
-        self.downloadedPhoto.layer.shadowOffset = CGSize(width: 0, height: 2)
+        downloadedPhoto.layer.shadowOpacity = 0.45
+        downloadedPhoto.layer.shadowRadius = 8
+        downloadedPhoto.layer.shadowOffset = CGSize(width: 0, height: 2)
     }
 }
