3b195895bbdffd4ced8969e96252a751979f4afa
Leonardo <leorazzo@gmail.com>
2018-04-03 16:26:13 +0200

    Updated Gitignore

---
+++ LikeApp/JSONWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  JSONWrapper.swift
+//  LikeApp
+//
+//  Created by Leonardo Razovic on 03/04/18.
+//  Copyright © 2018 Leonardo Razovic. All rights reserved.
+//
+
+import UIKit
+
+class JSONWrapper: NSObject {
+
+}
